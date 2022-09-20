2afd2aa6b11163ce2df118a817f5d5c6ac43cf38
mayangputri04 <113412622+mayangputri04@users.noreply.github.com>
2022-09-20 14:27:51 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Mayang Putri Emaliana
+           <br> Saya tinggal di daerah bekasi
+           <br> Olahraga yang saya sukai adalah Bulutangkis";
+
+    }
+}
